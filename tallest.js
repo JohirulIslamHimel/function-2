76c82bc84a001d7bce3c80a0f3a33b25416ab68f
Johirul Islam Himel <himel1199@gmail.com>
2026-02-02 21:46:45 +0600

    index changed for tallest.js

--- tallest.js
+++ tallest.js
@@ -1,6 +1,6 @@
 const heights = [65, 66, 68, 72, 78, 60, 65, 66];
 function getMax(numbers) {
-  let max = numbers[2];
+  let max = numbers[0];
   for (const num of numbers) {
     if (num > max) {
       max = num;
